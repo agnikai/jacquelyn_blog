b11c894984747a550471b40adf47bd863407413d
Jacquelyn Carver <jacquelyn905@gmail.com>
2016-02-05 00:39:55 -0500

    Added homepage header background image and shrink effect to image on scroll.

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,3 +1,13 @@
 //= require jquery/dist/jquery
 //= require bootstrap-sass/assets/javascripts/bootstrap
 //= require_tree .
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1){  
+        $('.header').addClass("sticky");
+        $('.recent_articles').addClass("show");
+    }
+    else{
+        $('.header').removeClass("sticky");
+    }
+});
